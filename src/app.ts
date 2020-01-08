79770b5e243cecfa00f15e82b7ffab8a1b580202
Michal Grzelak <mgrzelak@ahoy.com>
2020-01-08 18:20:41 +0100

    remove mongo

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,11 @@ import compression from "compression";  // compresses requests
 import session from "express-session";
 import bodyParser from "body-parser";
 import lusca from "lusca";
-import mongo from "connect-mongo";
 import flash from "express-flash";
 import path from "path";
-import mongoose from "mongoose";
 import passport from "passport";
-import bluebird from "bluebird";
-import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
+import { SESSION_SECRET } from "./util/secrets";
 
-const MongoStore = mongo(session);
 
 // Controllers (route handlers)
 import * as homeController from "./controllers/home";
@@ -26,16 +22,6 @@ import * as passportConfig from "./config/passport";
 // Create Express server
 const app = express();
 
-// Connect to MongoDB
-const mongoUrl = MONGODB_URI;
-mongoose.Promise = bluebird;
-
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } ).then(
-    () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
-).catch(err => {
-    console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
-    // process.exit();
-});
 
 // Express configuration
 app.set("port", process.env.PORT || 3000);
@@ -47,11 +33,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
     resave: true,
     saveUninitialized: true,
-    secret: SESSION_SECRET,
-    store: new MongoStore({
-        url: mongoUrl,
-        autoReconnect: true
-    })
+    secret: SESSION_SECRET
 }));
 app.use(passport.initialize());
 app.use(passport.session());
