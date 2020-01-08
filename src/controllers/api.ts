8bf1c7a0e39c630936fdd953b2216adf3490f4e7
Michal Grzelak <mgrzelak@ahoy.com>
2020-01-08 19:07:31 +0100

    minimum project

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,8 +1,6 @@
 "use strict";
 
-import graph from "fbgraph";
-import { Response, Request, NextFunction } from "express";
-import { UserDocument } from "../models/User";
+import { Response, Request, } from "express";
 
 
 /**
@@ -10,24 +8,11 @@ import { UserDocument } from "../models/User";
  * List of API examples.
  */
 export const getApi = (req: Request, res: Response) => {
-    res.render("api/index", {
-        title: "API Examples"
-    });
+    res.json({ answer: 42 });
 };
 
 /**
  * GET /api/facebook
  * Facebook API example.
  */
-export const getFacebook = (req: Request, res: Response, next: NextFunction) => {
-    const user = req.user as UserDocument;
-    const token = user.tokens.find((token: any) => token.kind === "facebook");
-    graph.setAccessToken(token.accessToken);
-    graph.get(`${user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err: Error, results: graph.FacebookUser) => {
-        if (err) { return next(err); }
-        res.render("api/facebook", {
-            title: "Facebook API",
-            profile: results
-        });
-    });
-};
+
